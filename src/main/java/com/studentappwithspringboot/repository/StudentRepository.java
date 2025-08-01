3f06271649b793dae9c0aadd38df5da71d3da708
gerardo-lopez-dev <gerardo.a.lopez1994@gmail.com>
2025-08-01 17:56:30 -0300

    probamos el pre commit

--- src/main/java/com/studentappwithspringboot/repository/StudentRepository.java
+++ src/main/java/com/studentappwithspringboot/repository/StudentRepository.java
@@ -1,8 +1,9 @@
 package com.studentappwithspringboot.repository;
 
 import com.studentappwithspringboot.model.Student;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface studentRepository extends JpaRepository<Student, Long> {
-
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findById(Long id);
 }
--- src/main/java/com/studentappwithspringboot/service/StudentService.java
+++ src/main/java/com/studentappwithspringboot/service/StudentService.java
@@ -1,3 +1,28 @@
 package com.studentappwithspringboot.service;
 
-public class StudentService extends IStudentService{}
+import com.studentappwithspringboot.model.Student;
+import com.studentappwithspringboot.repository.StudentRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService implements IStudentService {
+  private StudentRepository studentRepository;
+
+  @Override
+  public List<Student> getAllStudents() {return studentRepository.findAll();}
+
+  @Override
+  public Student findStudentById(Long id) {
+    Optional<Student> student = studentRepository.findById(id);return (student.orElse(null));
+  }
+
+  @Override
+  public Student saveStudent(Student student) {
+    return null;
+  }
+
+  @Override
+  public void deleteStudent(Student student) {}
+}
