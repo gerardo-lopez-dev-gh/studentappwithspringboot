7abc82866c3e62bf163bc404725bc3107978ddbc
gerardo-lopez-dev <gerardo.a.lopez1994@gmail.com>
2025-08-06 12:42:39 -0300

    Refactor main application to implement CommandLineRunner and add logging - Implemented CommandLineRunner in StudentappwithspringbootApplication - Added SLF4J logging for application startup and run method execution - Injected IStudentService with @Autowired - Minor code cleanup and added a sample console output

--- src/main/java/com/studentappwithspringboot/StudentappwithspringbootApplication.java
+++ src/main/java/com/studentappwithspringboot/StudentappwithspringbootApplication.java
@@ -1,13 +1,30 @@
 package com.studentappwithspringboot;
 
+import com.studentappwithspringboot.service.IStudentService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
 
 @SpringBootApplication
-public class StudentappwithspringbootApplication {
+public class StudentappwithspringbootApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentappwithspringbootApplication.class, args);
-	}
+  @Autowired private IStudentService studentService;
+  private static final Logger looger =
+      LoggerFactory.getLogger(StudentappwithspringbootApplication.class);
+  String ln = System.lineSeparator();
 
+  public static void main(String[] args) {
+    looger.info("Starting the aplication");
+    SpringApplication.run(StudentappwithspringbootApplication.class, args);
+    looger.info("Application started successfully");
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    looger.info("Executing the run method");
+    System.out.println("Hola, mundo");
+  }
 }
